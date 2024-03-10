3455c88c1da18f40965be7fe899d04584de24862
yanyanmaru <manmarutech@gmail.com>
2024-03-10 21:39:58 +0900

    音声ファイルを足した

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import Player from './../components/Player'
 export default function Home() {
   return (
     <main className='mt-8 flex justify-center gap-8'>
-      <Player file='first.mp3' />
-      <Player file='first.mp3' />
-      <Player file='first.mp3' />
-      <Player file='first.mp3' />
-      <Player file='first.mp3' />
+      <Player file='night.mp3' />
+      <Player file='fire.mp3' />
+      <Player file='hurin.mp3' />
+      <Player file='snow.mp3' />
+      <Player file='storm.mp3' />
     </main>
   );
 }
