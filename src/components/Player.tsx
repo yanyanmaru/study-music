b49332e913aa71b74c4b70d20ac5fd539a80ce42
yanyanmaru <manmarutech@gmail.com>
2024-03-10 22:26:39 +0900

    fontawesomeiconを追加した

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,6 +1,14 @@
 import React, { useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHouseChimney,faGamepad } from "@fortawesome/free-solid-svg-icons";
+library.add(faHouseChimney, faGamepad);
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fas, far, fab);
 
-export default function Player({file}) {
+export default function Player({file,icon}) {
   const [wavFile, setWavFile] = useState("first.mp3");
   const [isPlaying, setIsPlaying] = useState(false);
   const [volumeNumber, setVolumeNumber] = useState(0.5);
@@ -24,7 +32,7 @@ export default function Player({file}) {
 
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center relative">
     <div 
     onClick={togglePlay} 
     className={`${isPlaying ? "bg-gray-300" : ""} bg-gray-100 w-24 h-24 p-4 rounded flex items-center justify-center`}
@@ -38,8 +46,11 @@ export default function Player({file}) {
       >
         <source src={wavFile} type="audio/mpeg" />
       </audio>
-      
+      <FontAwesomeIcon className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" icon={icon} />
     </div>
+   
+    
+    <p>fafafa</p>
     <input
         type="range"
         min="0"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Player from './../components/Player'
 
+
 export default function Home() {
   return (
     <main className='mt-8 flex justify-center gap-8'>
-      <Player file='night.mp3' />
-      <Player file='fire.mp3' />
-      <Player file='hurin.mp3' />
-      <Player file='snow.mp3' />
-      <Player file='storm.mp3' />
+      <Player file='night.mp3' icon={["fas", "kiwi-bird"]} />
+      <Player file='fire.mp3' icon={["fas", "fire"]} />
+      <Player file='hurin.mp3' icon={["far", "bell"]} />
+      <Player file='snow.mp3' icon={["fas", "poo-storm"]} />
+      <Player file='storm.mp3' icon={["fas", "umbrella"]} />
     </main>
   );
 }
